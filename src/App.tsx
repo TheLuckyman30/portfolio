051670818082af7b361cf06eb6f7383011453307
TheLuckyman30 <bnauta0@gmail.com>
2026-03-02 19:39:22 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="h-screen w-screen flex justify-center items-center text-xl">
+      Hello World!
+    </main>
+  );
+}
+
+export default App;
