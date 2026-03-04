df524e866d81776fec5881ec88dcb62bea09b133
TheLuckyman30 <bnauta0@gmail.com>
2026-03-04 13:51:21 -0500

    Added a custom text button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import MyButton from "./components/MyButton";
+import MyTextButton from "./components/MyTextButon";
 
 function App() {
   return (
-    <main className="h-screen w-screen flex justify-center items-center text-xl">
+    <main className="flex-col h-screen w-screen flex justify-center items-center text-xl">
       <MyButton>Hello World!</MyButton>
+      <MyTextButton>Hello World!</MyTextButton>
     </main>
   );
 }
