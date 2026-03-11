3899831dff94e36e5b6e356c3167f9b60a0b2f10
TheLuckyman30 <bnauta0@gmail.com>
2026-03-11 02:19:36 -0400

    Modified sidebar styling

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,12 @@ import Projects from "./pages/projects/Projects";
 import Resume from "./pages/resume/Resume";
 import ContactMe from "./pages/contact-me/ContactMe";
 
-const PAGES = [
+interface Page {
+  name: string;
+  element: JSX.Element;
+}
+
+const PAGES: Page[] = [
   { name: "About Me", element: <AboutMe /> },
   { name: "Projects", element: <Projects /> },
   { name: "Resume", element: <Resume /> },
@@ -14,19 +19,24 @@ const PAGES = [
 ];
 
 function App() {
-  const [currentPage, setCurrentPage] = useState<JSX.Element>(PAGES[0].element);
+  const [currentPage, setCurrentPage] = useState<Page>(PAGES[0]);
 
   return (
     <main className="flex justify-center items-center h-screen w-screen bg-gray-200">
       <section className="bg-white rounded-md shadow-2xl p-5 h-[95%] w-[85%] flex">
         <Sidebar>
           {PAGES.map((page) => (
-            <MyTextButton onClick={() => setCurrentPage(page.element)}>
+            <MyTextButton onClick={() => setCurrentPage(page)}>
               {page.name}
             </MyTextButton>
           ))}
         </Sidebar>
-        {currentPage}
+        <section className="w-full h-full">
+          <div className="w-full text-center text-4xl text-blue-500 font-bold border-b-3 border-b-blue-300">
+            {currentPage.name}
+          </div>
+          <div className="p-2 h-full w-full">{currentPage.element}</div>
+        </section>
       </section>
     </main>
   );
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,6 @@
 export function Sidebar({ children }: { children?: React.ReactNode }) {
   return (
-    <nav className="flex flex-col gap-15 p-2 border-r-3 border-blue-300 w-fit h-full">
+    <nav className="flex flex-col gap-15 p-2 pr-5 border-r-3 border-blue-300 w-fit h-full">
       {children}
     </nav>
   );
