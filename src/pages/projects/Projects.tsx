9d4280e0d2d037e27db7d124aa48da63714bad37
TheLuckyman30 <bnauta0@gmail.com>
2026-03-17 01:20:04 -0400

    Initial project data

--- src/pages/projects/Projects.tsx
+++ src/pages/projects/Projects.tsx
@@ -1,3 +1,24 @@
+import type { MyProject } from "../../utils/interfaces";
+
+const myProjects: MyProject[] = [
+  {
+    name: "Dyslexease",
+    description:
+      "A full-stack web app that allows users to input their documents and quickly make dyslexic friendly modifications.",
+    techUsed: [
+      "Typescript",
+      "React",
+      "NestJs",
+      "Prsima",
+      "Postgres",
+      "Zustand",
+    ],
+    tags: [],
+    siteLink: "",
+    githubLink: "https://github.com/selinAbacaz/CapstoneDyslexease",
+  },
+];
+
 export function Projects() {
   return <div>Projects</div>;
 }
---
+++ src/utils/interfaces/MyProject.ts
@@ -0,0 +1,8 @@
+export interface MyProject {
+  name: string;
+  description: string;
+  techUsed: string[];
+  tags: [];
+  siteLink: string;
+  githubLink: string;
+}
