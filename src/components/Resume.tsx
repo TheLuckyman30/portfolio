84cc4ba9f1204175125924db15640af504d9d7d2
TheLuckyman30 <bnauta0@gmail.com>
2026-04-11 19:40:57 -0400

    Initial resume modal implemented

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -29,6 +29,25 @@ export function Resume({ show, setShow }: ResumeProps) {
               X
             </button>
           </div>
+          <div className="w-fit bg-white p-1 rounded-md shadow-md hover:scale-105 duration-75">
+            <a href={resume} download="bnauta-resume.pdf">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path d="M12 15V3" />
+                <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
+                <path d="m7 10 5 5 5-5" />
+              </svg>
+            </a>
+          </div>
           <div className="rounded-md overflow-hidden">
             <Document file={resume}>
               <Page pageNumber={1} />
