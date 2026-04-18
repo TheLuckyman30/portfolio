4e6e0850d8ac3e8fae5f498f28e324f3e707f584
TheLuckyman30 <bnauta0@gmail.com>
2026-04-18 19:20:17 -0400

    Project page is now more responsive

--- src/pages/PageOutline.tsx
+++ src/pages/PageOutline.tsx
@@ -7,11 +7,13 @@ interface PageProps {
 
 export function PageOutline({ pageName, pageElement }: PageProps) {
   return (
-    <section className="flex flex-col w-full h-full">
-      <div className="pt-2 pb-2 w-full text-center text-4xl text-blue-700 font-bold">
+    <section className="flex flex-col h-full w-full">
+      <div className="py-2 w-full text-center text-4xl text-blue-700 font-bold">
         {pageName}
       </div>
-      <div className="p-5 h-full w-full overflow-y-hidden">{pageElement}</div>
+      <section className="p-5 w-full grow overflow-y-auto">
+        {pageElement}
+      </section>
     </section>
   );
 }
--- src/pages/projects/components/ProjectCard.tsx
+++ src/pages/projects/components/ProjectCard.tsx
@@ -8,24 +8,17 @@ interface ProjectPanelProps {
 export function ProjectCard({ project }: ProjectPanelProps) {
   return (
     <div className="flex flex-col w-100 min-h-100 border border-gray-300 rounded-md shadow-md overflow-hidden">
-      <div
-        className="w-full h-40 shrink-0"
-        style={{ backgroundImage: `url(${pic})` }}
-      />
-      <div className="flex flex-col w-full grow">
-        <div className="p-2 text-xl text-gray-700">{project.name}</div>
-        <div className="p-2 pl-5 pr-5 grow text-sm">{project.description}</div>
-        <div className="flex flex-wrap gap-2 p-2">
-          {project.techUsed.map((tech) => (
-            <div className="p-1 text-xs bg-gray-50 border border-gray-300 rounded-md text-gray-700 text-center">
-              {tech}
-            </div>
-          ))}
-        </div>
-        <div className="p-2 border-t border-gray-300 bg-gray-50">
-          Links here
-        </div>
+      <div className="w-full h-40" style={{ backgroundImage: `url(${pic})` }} />
+      <div className="p-2 text-xl text-gray-700">{project.name}</div>
+      <div className="p-2 px-5 grow text-sm">{project.description}</div>
+      <div className="flex flex-wrap gap-2 p-2">
+        {project.techUsed.map((tech) => (
+          <div className="p-1 text-xs bg-gray-50 border border-gray-300 rounded-md text-gray-700 text-center">
+            {tech}
+          </div>
+        ))}
       </div>
+      <div className="p-2 border-t border-gray-300 bg-gray-50">Links here</div>
     </div>
   );
 }
