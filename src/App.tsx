0c8d3ef9869a4bb92f16222cd2b87fcda7a3dc42
TheLuckyman30 <bnauta0@gmail.com>
2026-04-19 13:51:30 -0400

    Added custom fonts

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,7 @@ function App() {
   ];
 
   return (
-    <main>
+    <main className="font-roboto">
       <Navbar>
         {pages.map((page) => (
           <TextButton onClick={() => setCurrentPage(page)}>
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -8,7 +8,7 @@ const sections = [
 export function Home() {
   return (
     <section className="flex flex-col justify-center items-center p-5">
-      <section className="flex gap-5 items-center min-h-screen">
+      <section className="flex font-montserrat gap-5 items-center min-h-screen">
         <div className="rounded-full h-55 w-55 border border-blue-700 bg-gray-200" />
         <div>
           <p className="font-semibold text-6xl">Brandon Nauta</p>
@@ -18,7 +18,7 @@ export function Home() {
       <section className="flex flex-col gap-50 w-[40%] h-fit">
         {sections.map((section) => (
           <div className="flex flex-col gap-10">
-            <h1 className="font-semibold text-center text-3xl text-blue-500">
+            <h1 className="font-semibold font-montserrat text-center text-3xl text-blue-500">
               {section.name}
             </h1>
             {section.element}
