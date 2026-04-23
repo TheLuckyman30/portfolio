7def0428e189bb1021cb279771a9f7aedb1feef4
TheLuckyman30 <bnauta0@gmail.com>
2026-04-23 03:24:12 -0400

    Tweaked filter function so it no longer uses useEffect

--- src/pages/home/sections/FeaturedProjects.tsx
+++ src/pages/home/sections/FeaturedProjects.tsx
@@ -1,31 +1,23 @@
 import { ProjectCard } from "../../projects/components";
 import { Input } from "../../../components";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import projects from "../../../data/my-projects.json";
-import { type MyProject } from "../../../utils/interfaces";
 
 export function FeaturedProjects() {
   const [searchValue, setSearchValue] = useState<string>("");
-  const [filteredProjects, setFilteredProjects] =
-    useState<MyProject[]>(projects);
 
-  useEffect(() => {
-    const newProjects = projects.filter((project) => {
-      if (!searchValue) {
-        return project;
-      }
+  const filteredProjects = projects.filter((project) => {
+    const description = project.description.toLowerCase();
+    const techUsed = project.techUsed.join(",").toLowerCase();
+    const title = project.name.toLowerCase();
 
-      /// const description = project.description.toLowerCase(); Might still check description
-      const techUsed = project.techUsed.join(",").toLowerCase();
-      const title = project.name.toLowerCase();
-
-      if (title.includes(searchValue) || techUsed.includes(searchValue)) {
-        return project;
-      }
-    });
-
-    setFilteredProjects(newProjects);
-  }, [searchValue]);
+    return (
+      !searchValue &&
+      (title.includes(searchValue) ||
+        techUsed.includes(searchValue) ||
+        description.includes(searchValue))
+    );
+  });
 
   return (
     <div className="flex flex-col gap-5">
