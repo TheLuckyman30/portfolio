5117d98e3c22e8bf60d289fc98d4620bc3d59329
TheLuckyman30 <bnauta0@gmail.com>
2026-04-25 23:21:49 -0400

    Adjusted width of inner navbar element

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 export function Navbar({ children }: { children?: React.ReactNode }) {
   return (
-    <nav className="font-montserrat flex fixed border-b bg-white/50 backdrop-blur-sm border-gray-300 w-screen h-fit shadow-md justify-center items-center">
-      <div className="flex gw-full sm:max-w-5xl p-3 gap-15 justify-center items-center">
+    <nav className="font-montserrat fixed border-b bg-white/50 backdrop-blur-sm border-gray-300 w-full h-fit shadow-md">
+      <div className="flex gw-full mx-auto sm:max-w-5xl p-3 gap-15 justify-center items-center">
         {children}
       </div>
     </nav>
