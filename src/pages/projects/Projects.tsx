56b711a77af81d01d0dc8da9f7f905eccd6b2e87
TheLuckyman30 <bnauta0@gmail.com>
2026-04-26 03:25:50 -0400

    Tweaked search bar on all projects page so it stays in place if no data & added title to page

--- src/pages/projects/Projects.tsx
+++ src/pages/projects/Projects.tsx
@@ -9,18 +9,28 @@ export function Projects() {
   const filteredProjects = filterProjects(projects, searchValue);
 
   return (
-    <section className="flex flex-col justify-center items-center min-h-screen pt-20 gap-10 p-5">
-      <div className="flex w-fit">
-        <Input
-          placeholder="Search"
-          onChange={(e) => setSearchValue(e.target.value.toLowerCase())}
-        />
+    <section className="min-h-screen pt-20 p-5">
+      <div className="flex flex-col justify-center items-center gap-5 w-full">
+        <h1 className="text-blue-500 font-bold text-4xl">All Projects</h1>
+        <div className="flex w-fit mb-5">
+          <Input
+            placeholder="Search"
+            onChange={(e) => setSearchValue(e.target.value.toLowerCase())}
+          />
+        </div>
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 2xl:grid-cols-3 gap-20">
-        {filteredProjects.map((project) => (
-          <ProjectCard project={project} />
-        ))}
-      </div>
+      <section className="flex w-full justify-center items-center">
+        {filteredProjects.length !== 0 && (
+          <div className="grid grid-cols-1 sm:grid-cols-2 2xl:grid-cols-3 gap-20">
+            {filteredProjects.map((project) => (
+              <ProjectCard project={project} />
+            ))}
+          </div>
+        )}
+        {filteredProjects.length === 0 && (
+          <span className=" text-gray-500">No Projects Found!</span>
+        )}
+      </section>
     </section>
   );
 }
