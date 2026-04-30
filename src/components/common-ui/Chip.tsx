cfb6589f824c2340f677ae09eea197273a0eb85b
TheLuckyman30 <bnauta0@gmail.com>
2026-04-29 20:40:41 -0400

    Added background to skills section

--- src/components/common-ui/Chip.tsx
+++ src/components/common-ui/Chip.tsx
@@ -19,10 +19,14 @@ export function Chip({
     lg: "text-lg",
   };
 
+  const colorClasses = {
+    primary: "bg-slate-700 border-slate-950 text-white",
+  };
+
   return (
     <div
       {...divProps}
-      className={`p-1 ${sizeClasses[size]} bg-gray-50 border border-gray-300 rounded-md text-gray-700 text-center sm:whitespace-nowrap`}
+      className={`p-1 border font-medium shdadow-md rounded-md text-center sm:whitespace-nowrap hover:scale-110 duration-75 ${sizeClasses[size]} ${colorClasses[variant]}`}
     >
       {children}
     </div>
